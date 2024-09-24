8400213ecd0c54eaf1163bc26af5b701d13a419a
anonymoustest02 <sufssl02@gmail.com>
2024-09-24 13:09:09 -0500

    Create sleep.sh

---
+++ sleep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Print a message indicating that the script is about to sleep
+echo "Sleeping for 30 seconds..."
+
+# Sleep for 30 seconds
+sleep 30
+
+# Print a message indicating that the sleep period is over
+echo "Done sleeping for 30 seconds."
